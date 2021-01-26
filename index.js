9914b287eec5c53075b70228330c9e7fdee77450
Shilcof <robert.shilcof@gmail.com>
2021-01-26 12:28:26 +0000

    commit tree 956abc5a1ded1316a2194b3a3b77f19881c8e261

--- index.js
+++ index.js
@@ -10,4 +10,17 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  const timeNumber = parseInt(timeString.split(':')[0], 10);
+  if (timeNumber < 12) {
+    return "Good Morning";
+  } else if (timeNumber < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  document.querySelector("h1").innerText = string;
+}
